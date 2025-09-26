22e951b0efea544c684638f609fc96d6d0f0b1ab
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-09-26 15:47:19 +1000

    Changed the password check program to use functions.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,14 +2,23 @@
 CP1404
 Program that asks the user for a password and then prints it back as a line of stars.
 """
+#Start the main function
+def main():
+    MIN_PASSWORD_LENGTH = 6
+    password = get_password(MIN_PASSWORD_LENGTH)
 
 
-min_password_length = 6
+def get_password(MIN_PASSWORD_LENGTH):
+    user_password = input("What is your password? ")
+    while len(user_password) < MIN_PASSWORD_LENGTH:
+        print(f"Your password must be at least {MIN_PASSWORD_LENGTH} characters long.")
+        user_password = input("What is your password? ")
 
-password = input("What is your password: ")
+    star_printer(len(user_password))
 
-while len(password) < min_password_length:
-    print(f"Your password must be at least {min_password_length} characters long.")
-    password = input("What is your password: ")
+#Function that converts the user's password to stars
+def starprinter(star_length):
+    print("*" * star_length)
 
-print("*" * len(password))
+#Call the main function
+main()
