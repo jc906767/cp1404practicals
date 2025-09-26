d9c983c0c3dbef2747b94118cccd6080afb6be81
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-09-26 16:49:59 +1000

    Added a random score generator to the score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,17 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
-
+import random
 
 def main():
     score = float(input("What is your score?  "))
     result = determine_score_status(score)
     print(result)
+
+    #Section of code that generates the random score
+    random_score = random.uniform(0, 100)
+    print(f"Random score: {random_score:.2f}")
+    print(determine_score_status(random_score))
 
 def determine_score_status(score):
     if score < 0 or score > 100:
