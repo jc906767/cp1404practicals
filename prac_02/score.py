706802f89cf0387d87baf3b10d4e471cee1b8960
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-09-26 16:35:50 +1000

    Changed the score program to use functions.

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,20 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("What is your score?  "))
+    result = determine_score_status(score)
+    print(result)
+
+def determine_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
