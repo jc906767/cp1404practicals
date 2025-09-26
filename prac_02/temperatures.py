75cbc2b25d76ab555f01e67fc4808169f6158081
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-09-26 16:28:19 +1000

    Added some comments

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -3,6 +3,7 @@ CP1404/CP5632 - Practical
 Program for temperature conversion
 """
 
+#Main function where the menu is shown and conversion choices are made
 def main():
     MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
@@ -17,17 +18,17 @@ Q - Quit"""
             fahrenheit = float(input("Fahrenheit: "))
             print(fahrenheit_to_celsius(fahrenheit))
         else:
-            print("Invalid option")
+            print("Invalid option, please choose an option from the menu")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
 
-
+#Function that converts Celcius to Fahrenheit
 def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
     return f"Result: {fahrenheit:.2f} F"
 
-
+#Function that converts Fahrenheit to Celcius
 def fahrenheit_to_celsius(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
     return f"Result: {celsius:.2f} C"
