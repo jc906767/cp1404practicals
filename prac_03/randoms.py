7a62f872f3d2fd73cbd4d3f79d2a3f33b8d8d959
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-03 12:21:03 +1000

    Completed the randoms prac 3 tasks

---
+++ prac_03/randoms.py
@@ -0,0 +1,23 @@
+
+#What did you see on line 1?
+#10, 16, 10, 20 and 19
+#What was the smallest number you could have seen, what was the largest?
+#The smallest number that could have been seen was 5 and the largest was 20.
+
+
+#What did you see on line 2?
+#9, 5, 9, 5 and 7
+#What was the smallest number you could have seen, what was the largest?
+#The smallest number that could have been seen was 3 and the largest was 9.
+#Could line 2 have produced a 4?
+#No it can only produce odd numbers.
+
+#What did you see on line 3?
+# 4.348346105981366, 5.4679197577952845, 3.689265818895244, 2.5196950112168675 and 4.662532159716587
+#What was the smallest number you could have seen, what was the largest?
+#The smallest number that could have been seen was 2.5 and the largest was 5.499999... .
+
+#Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+random_number = random.randint(1, 100)
+print(random_number)
