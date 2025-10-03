ca3d1a8b9868bbcf149dc48ecb53aa0e1300ea87
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-03 16:31:10 +1000

    Completed final to do

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,27 +5,28 @@ Files exercises
 
 # 1. Ask user for their name, write it to name.txt
 name = input("Enter your name: ")
-out_file = open("name.txt", "w")
-print(name, file=out_file)
-out_file.close()
+output_file = open("name.txt", "w")
+print(name, file=output_file)
+output_file.close()
 
 
 # 2. Read the name from name.txt and print it
-in_file = open("name.txt", "r")
-name = in_file.read().strip()
-in_file.close()
+input_file = open("name.txt", "r")
+name = input_file.read().strip()
+input_file.close()
 print(f"Hi {name}!")
 
 
 # 3. Create numbers.txt with these lines: 17, 42, 400
 # Then read only the first two numbers, add them, and print result
-with open("numbers.txt", "r") as in_file:
-    number1 = int(in_file.readline())
-    number2 = int(in_file.readline())
+with open("numbers.txt", "r") as input_file:
+    number1 = int(input_file.readline())
+    number2 = int(input_file.readline())
     print(number1 + number2)
 
 
-# TODO: 4) ow write a fourth block of code that prints the total for all
-#  lines in numbers.txt. This should work for a file with any number of
-#  numbers. Use with instead of open and close for this question.
-
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        total += int(line)
+print(total)
