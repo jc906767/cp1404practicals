18d571049c612cd9b16d7b2e632076624321e2ba
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-03 13:57:55 +1000

    Answered the given questions

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -1,9 +1,6 @@
 """
 CP1404/CP5632 - Practical
 Answer the following questions:
-1. When will a ValueError occur?
-2. When will a ZeroDivisionError occur?
-3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
 try:
@@ -16,3 +13,12 @@ except ValueError:
 except ZeroDivisionError:
     print("Cannot divide by zero!")
 print("Finished.")
+
+#1. When will a ValueError occur?
+#If something that cannot be converted to an interger as either the numerator or denominator, like letters and imaginary numbers.
+
+#2. When will a ZeroDivisionError occur?
+# If the denominator is chosen to be 0.
+
+#3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+# Yes you could. You would need to check the denominator is not 0 before completing the division.
