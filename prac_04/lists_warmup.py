73feac95d4013f6096eb6b3c53624c2c5737a92f
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-09 14:21:57 +1000

    Completed the remaining questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -16,3 +16,29 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # "3" in numbers      = False
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+print(f"numbers before changing anything: {numbers}")
+
+#1 Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
+print(f"numbers after changing first element: {numbers}")
+
+#2 Change the last element of numbers to 1
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[-1] = 1
+print(f"numbers after changing the last element: {numbers}")
+
+#3 Print all the elements from numbers except the first two (slice)
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers = numbers[slice(2, None)]
+print(f"numbers after removing the first 2 elements: {numbers}")
+
+
+#4 Print whether 9 is an element of numbers
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+if 9 in numbers:
+    print(f"9 is in: {numbers}")
+else:
+    print(f"9 is not in: {numbers}")
+
