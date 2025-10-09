4d671ae8ad3576bc88c92a28fcaa33e49ca9d936
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-09 15:20:52 +1000

    Completed things to do step 2

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,16 +14,20 @@ def main():
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    all_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        all_data.append(parts)
 
+        #print(line)  # See what a line looks like
+        #print(repr(line))  # See what a line really looks like
+        #print(parts)  # See what the parts look like (notice the integer is a string)
+        #print(parts)  # See if that worked
+        #print("----------")
+
+    input_file.close()
+    return all_data
 
 main()
