0535f159ad88aa0bbcdf06ebd77e771a65d3693e
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-09 15:31:28 +1000

    Completed code for subject_reader

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,20 +7,20 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
-
+    nested_info = load_data(FILENAME)
+    display_subject_details(nested_info)
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
-    all_data = []
+    nests = []
     for line in input_file:
         line = line.strip()
         parts = line.split(',')
         parts[2] = int(parts[2])
-        all_data.append(parts)
+        nests.append(parts)
 
+        # Parts removed to complete Things to do step 2
         #print(line)  # See what a line looks like
         #print(repr(line))  # See what a line really looks like
         #print(parts)  # See what the parts look like (notice the integer is a string)
@@ -28,6 +28,10 @@ def load_data(filename=FILENAME):
         #print("----------")
 
     input_file.close()
-    return all_data
+    return nests
 
+def display_subject_details(nested_info):
+    """Display subject details."""
+    for subject_info in nested_info:
+        print(f"{subject_info[0]} is taught by {subject_info[1]} and has {subject_info[2]} students")
 main()
