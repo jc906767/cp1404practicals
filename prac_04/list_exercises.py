0614a94d6362ee5d5530c5179c48b68d53712b42
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-10-11 15:38:46 +1000

    Completed the woefully inadequate security checker task

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,26 +1,46 @@
 """
 CP1404 Practical
-Basic list operations
+Basic list operations and woefully inadequate security checker.
 """
 
+NUMBER_COUNT = 5
 
 def main():
+    """Run the number sorter and security checker."""
+    numbers = get_numbers(NUMBER_COUNT)
+    display_number_summary(numbers)
+    check_user_access()
+
+def get_numbers(count):
     """request 5 numbers, store them in a list, and display summary information."""
     numbers = []
-    NUMBER_COUNT = 5
-
-    for i in range(NUMBER_COUNT):
-        number = int(input("Number: "))
+    print("Please give 5 values")
+    for i in range(count):
+        number = int(input(f"Input number {i + 1}: "))
         numbers.append(number)
-
-    display_number_summary(numbers)
+    return numbers
 
 def display_number_summary(numbers):
-    """Display all the numbers."""
-    print(f"The first number is {numbers[0]}")
-    print(f"The last number is {numbers[-1]}")
-    print(f"The smallest number is {min(numbers)}")
-    print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+    """Display the first, last, smallest, largest, and average of the numbers."""
+    print()
+    for i, number in enumerate(numbers, start=1):
+        print(f"Number {i}: {number}")
+    print(f"The first number is: {numbers[0]}")
+    print(f"The last number is: {numbers[-1]}")
+    print(f"The smallest number is: {min(numbers)}")
+    print(f"The largest number is: {max(numbers)}")
+    print(f"The average of the numbers is: {sum(numbers) / len(numbers):.1f}")
+
+def check_user_access():
+    """Ask for a username and print whether access is granted."""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+
+    username_input = input("Please type your username: ")
+    if username_input in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 main()
