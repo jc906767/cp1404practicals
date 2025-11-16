5f8bde201bac4de4dd496717d5577f2502519244
jc906767 <lachlan.blackney@my.jcu.edu.au>
2025-11-16 13:55:10 +1000

    Completed the box_layout_demo and box_layout files

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,3 +1,9 @@
+"""
+CP1404 - Practical 8
+Box Layout Demo
+Lachlan Blackney
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +13,15 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        """changes the text in the greeting box to the given name"""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """clears all the text from the output_label box"""
+        self.root.ids.output_label.text = ''
+
 
 
 BoxLayoutDemo().run()
